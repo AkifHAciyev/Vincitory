e631ab22d8fd86c7dca29fc49e13ea1c51b25bd7
AkifHaciyev <akif6563@mail.ru>
2023-05-07 18:40:19 +0400

    login

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -3,6 +3,7 @@ import PageLayout from '../components/PageLayout';
 import NotFound from '../pages/NotFound';
 import Kredit from '../pages/Kredit';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 
 const PageRoutes = () => {
 	return (
@@ -10,6 +11,7 @@ const PageRoutes = () => {
 			<Route element={<PageLayout />}>
 				<Route index element={<Home />} />
 				<Route path="/kredit" element={<Kredit />} />
+				<Route path="/login" element={<Login />} />
 				<Route path="*" element={<NotFound />} />
 			</Route>
 		</Routes>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,34 @@
+import styled from './index.module.scss';
+import bgImg from '../../assets/images/loginImg.svg';
+import rectangle from '../../assets/images/Rectangle.svg';
+
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div>
+			<div className={styled.left}>
+				<img src={bgImg} alt="loginImg" />
+				<img className={styled.rectangle} src={rectangle} alt="" />
+			</div>
+			<div className={styled.right}>
+				<h1 className={styled.title}>Hesaba daxil ol</h1>
+				<form>
+					<div className={styled.workBox}>
+						<label className={styled.workTitle}>Ad və soyadınız</label>
+						<input className={styled.workInput} type="text" placeholder="Ad və soyadınızı edin" />
+					</div>
+					<div className={styled.workBox}>
+						<label className={styled.workTitle}>Şifrə</label>
+						<input className={styled.workInput} type="text" placeholder="Şifrənizi daxil edin" />
+					</div>
+					<div>
+						<input type="checkbox" />
+						<label>Yadda saxla</label>
+					</div>
+					<button></button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
